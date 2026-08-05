89598b56874258cf2c7d20ac5982deb67dff75de
omkar274 <ksomkar62@gmail.com>
2026-08-05 22:27:03 +0530

    mergeintervals

---
+++ mergeintervals.py
@@ -0,0 +1,24 @@
+def merge(intervals):
+
+    intervals.sort()
+
+    result = [intervals[0]]
+
+    for current in intervals[1:]:
+
+        last = result[-1]
+
+        if current[0] <= last[1]:
+
+            last[1] = max(last[1], current[1])
+
+        else:
+
+            result.append(current)
+
+    return result
+
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+
+print(merge(intervals))
