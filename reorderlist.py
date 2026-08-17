11d8e70a27f971f47fa8cc3c1b2b139dc9bad819
omkar274 <ksomkar62@gmail.com>
2026-08-17 22:02:16 +0530

    reorderlist

---
+++ reorderlist.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+
+        # 1. Find the middle
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # 2. Reverse the second half
+        second = slow.next
+        slow.next = None
+
+        prev = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        # 3. Merge both halves
+        first = head
+        second = prev
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
