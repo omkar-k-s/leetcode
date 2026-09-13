12e66565038461bfa9a49a9659bae9028f22da5a
omkar274 <ksomkar62@gmail.com>
2026-09-13 21:50:28 +0530

    reversebit

---
+++ reversebit.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseBits(self, n):
+        result = 0
+
+        for i in range(32):
+            bit = n & 1
+            result = (result << 1) | bit
+            n = n >> 1
+
+        return result
