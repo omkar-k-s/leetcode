cb8d27b0398b0cfa911e68874043bdc833239b8f
omkar274 <ksomkar62@gmail.com>
2026-09-16 21:37:51 +0530

    climbingsatirs

---
+++ climibingstairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        one = 1
+        two = 2
+
+        for i in range(3, n + 1):
+            current = one + two
+            one = two
+            two = current
+
+        return two
